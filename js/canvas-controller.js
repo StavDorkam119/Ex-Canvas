1704d1e95c25e6c280af2a69f6810b7d0609512b
Stav Dorkam <stavforu119@gmail.com>
2019-06-05 17:01:32 +0300

    Fourth Commit

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -1,54 +1,70 @@
 'use strict'
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+const gCanvas = document.querySelector('#canvas');
+const gCtx = gCanvas.getContext('2d');
 
 function init() {
     getCanvasSettings();
 }
 
 function setCanvasSize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    gCanvas.width = window.innerWidth;
+    gCanvas.height = window.innerHeight;
 }
 
 
-function setCanvasColors() {
-    ctx.strokeStyle = getColor();
-    canvas.style.backgroundColor = getBackgroundColor();
-    ctx.fillStyle = getBackgroundColor();
+function renderCanvasColors() {
+    gCtx.strokeStyle = getColor();
+    gCanvas.style.backgroundColor = getBackgroundColor();
+    gCtx.fillStyle = getBackgroundColor();
 }
 
 function onChangeShape(shape) {
+    updateCanvasShape(shape);
     
 }
 
+function onChangeCanvasShapeColor(color) {
+    updateCanvasColor(color);
+    gCtx.strokeStyle = getColor();
+    // gCanvas.style.backgroundColor = getBackgroundColor();
+    // gCtx.fillStyle = getBackgroundColor();
+}
+
+function onChangeCanvasBackgroundColor(backgroundColor) {
+    updateCanvasBackgroundColor(backgroundColor);
+    // gCtx.strokeStyle = getColor();
+    gCanvas.style.backgroundColor = getBackgroundColor();
+    gCtx.fillStyle = getBackgroundColor();
+}
+
+
 function drawTriangle(x,y) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(300, 150);
-    ctx.lineTo(100, 100);
-    ctx.closePath()
-    ctx.lineWidth = 5;
+    gCtx.beginPath();
+    gCtx.moveTo(0, 0);
+    gCtx.lineTo(300, 150);
+    gCtx.lineTo(100, 100);
+    gCtx.closePath()
+    gCtx.lineWidth = 5;
     // ctx.strokeStyle = getColor();
     // ctx.fillStyle = '#ff0000'
-    ctx.stroke();
-    ctx.fill()
+    gCtx.stroke();
+    gCtx.fill()
  
 }
 
 function drawRect(x,y) {
-    ctx.rect(x,y, 150, 150)
+    gCtx.rect(x,y, 150, 150)
     // ctx.fillStyle = 'orange'
-    ctx.fillRect(x,y, 150, 150)
-    ctx.stroke()
-    ctx.fill()
+    gCtx.fillRect(x,y, 150, 150)
+    gCtx.stroke()
+    gCtx.fill()
 }
 
 function drawCircle(x,y) {
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
+    gCtx.beginPath();
+    gCtx.arc(100, 75, 50, 0, 2 * Math.PI);
+    gCtx.stroke();
 
 
 }
