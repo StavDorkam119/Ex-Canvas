8993165e550a9eb9ab3883d43abe40ca5b524df8
Stav Dorkam <stavforu119@gmail.com>
2019-06-05 18:21:09 +0300

    Shapes Added

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -5,6 +5,12 @@ const gCtx = gCanvas.getContext('2d');
 
 function init() {
     getCanvasSettings();
+    setCanvasSize();
+    setInitialSettings();
+    gCanvas.addEventListener('mousemove', ev => {
+        if (ev.buttons !== 1) return;
+        draw(ev);
+    })
 }
 
 function setCanvasSize() {
@@ -21,7 +27,6 @@ function renderCanvasColors() {
 
 function onChangeShape(shape) {
     updateCanvasShape(shape);
-    
 }
 
 function onChangeCanvasShapeColor(color) {
@@ -38,12 +43,35 @@ function onChangeCanvasBackgroundColor(backgroundColor) {
     gCtx.fillStyle = getBackgroundColor();
 }
 
+function draw(ev) {
+    const {offsetX, offsetY} = ev
+    switch(gCanvasSettings.currShape) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY);
+            break;
+        case 'circle': 
+            drawCircle(offsetX, offsetY);
+            break;
+        case 'rectangle': 
+            drawRect(offsetX, offsetY);
+            break;
+       }
+}
+
+
+function setInitialSettings() {
+    gCtx.strokeStyle = getColor();
+    gCanvas.style.backgroundColor = getBackgroundColor();
+    gCtx.fillStyle = getBackgroundColor();
+}
+
 
 function drawTriangle(x,y) {
+    gCtx.moveTo(x, y);
     gCtx.beginPath();
-    gCtx.moveTo(0, 0);
-    gCtx.lineTo(300, 150);
-    gCtx.lineTo(100, 100);
+    gCtx.lineTo(x-75, y-75);
+    gCtx.lineTo(x+75, y-75);
+    gCtx.lineTo(x, y+75);
     gCtx.closePath()
     gCtx.lineWidth = 5;
     // ctx.strokeStyle = getColor();
@@ -54,18 +82,14 @@ function drawTriangle(x,y) {
 }
 
 function drawRect(x,y) {
-    gCtx.rect(x,y, 150, 150)
-    // ctx.fillStyle = 'orange'
-    gCtx.fillRect(x,y, 150, 150)
-    gCtx.stroke()
-    gCtx.fill()
+    gCtx.strokeRect((x-75),(y-75), 150, 150);
+    // gCtx.fillRect((x-75),(y-75), 150, 150);
 }
 
 function drawCircle(x,y) {
     gCtx.beginPath();
-    gCtx.arc(100, 75, 50, 0, 2 * Math.PI);
+    gCtx.arc(x, y, 75, 0, 2 * Math.PI);
     gCtx.stroke();
-
-
+    // gCtx.fill();
 }
 
