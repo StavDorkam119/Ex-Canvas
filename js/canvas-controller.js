6589828c1a49f1f2363e796a34927d0808dabb0b
Maor Tovim <maortovim@gmail.com>
2019-06-05 15:53:43 +0300

    first-commit

---
+++ js/canvas-controller.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+function init() {
+    // setCanvasSize()
+}
+
+function setCanvasSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
