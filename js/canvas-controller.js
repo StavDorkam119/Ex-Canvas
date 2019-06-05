8fab104c4f1da70b4993e4e1422894d2f245e2b5
Maor Tovim <maortovim@gmail.com>
2019-06-05 16:35:28 +0300

    checking

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -11,3 +11,38 @@ function setCanvasSize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
+
+
+function setCanvasColors() {
+    ctx.strokeStyle = getColor();
+    canvas.style.backgroundColor = getBackgroundColor();
+    ctx.fillStyle = getBackgroundColor();
+}
+
+function drawTriangle(x,y) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(300, 150);
+    ctx.lineTo(100, 100);
+    ctx.closePath()
+    ctx.lineWidth = 5;
+    // ctx.strokeStyle = getColor();
+    // ctx.fillStyle = '#ff0000'
+    ctx.stroke();
+    ctx.fill()
+ 
+}
+
+function drawRect(x,y) {
+    ctx.rect(x,y, 150, 150)
+    // ctx.fillStyle = 'orange'
+    ctx.fillRect(x,y, 150, 150)
+    ctx.stroke()
+    ctx.fill()
+}
+
+function drawCircle(x,y) {
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+}
---
+++ js/canvas-service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+function getColor() {
+    gCanvasProps.color;
+}
+
+function getBackgroundColor() {
+    gCanvasProps.backgroundcolor;
+}
+
